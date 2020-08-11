9c0ed52b6a7f3ebb82372bed792535accb29d02e
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-11 16:51:01 -0700

    added new features

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { removeItem } from "../actions";
+import { connect } from "react-redux";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          props.removeItem(props.feature);
+        }}
+        className="button"
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeItem })(AddedFeature);
